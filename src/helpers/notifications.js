eb7acbecbcaec5c33face4767beff72a16efb126
Sergey Miniaylo <po4tovuj@gmail.com>
2022-10-27 18:25:17 +0300

    rework API_key

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -5,7 +5,3 @@ const options = {};
 export const onSuccessAlert = message => Notify.success(message, options);
 
 onErrorAlert = message => Notify.error(message, options);
-
-// const MAIN_API_KEY =  create new API
-
-const API_REY = ''; /*put your personal API-key instead of empty string*/
