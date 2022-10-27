87afdaa542c3c9c3b453c130924989127edbbc68
Roman Mokrushin <emptyspaceman1@gmail.com>
2022-10-27 13:56:21 +0300

    adds start files

---
+++ src/helpers/notifications.js
@@ -0,0 +1,9 @@
+const options = {};
+
+onSuccessAlert = message => Notify.success(message, options);
+
+onErrorAlert = message => Notify.error(message, options);
+
+// const MAIN_API_KEY =  create new API
+
+const API_REY = ''; /*put your personal API-key instead of empty string*/
