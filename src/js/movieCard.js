8b2a21074e94041eb9ca91f3962b34f4def791b7
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-10-29 15:32:18 +0300

    footer&movie-card

---
+++ src/js/movieCard.js
@@ -0,0 +1,54 @@
+// import Жанри
+import 'lazysizes';
+// import placeholderImg from 'Занйти Плейсхолдер';
+
+export function renderCards(data) {
+  const movieCardMarkup = data
+    ?.map(
+      ({
+        id,
+        title,
+        original_title,
+        overview,
+        popularity,
+        poster_path,
+        release_date,
+        vote_average,
+        genre_ids,
+      }) => {
+        // const genresNames = Жанри(genre_ids);
+        return `<li class="movie-card" data-id="${id}">
+  <div class="img-container">
+  ${
+    !poster_path
+      ? `<img
+      src="${placeholderImg}" alt="${title}" class="movie-card__img">`
+      : `<img
+    data-src="https://image.tmdb.org/t/p/w400/${poster_path}"
+    src="${placeholderImg}" alt="${title}" class="lazyload blur-up movie-card__img">`
+  }
+  </div>
+  <h2 class="movie-card__title">${title}</h2>
+  <div class="movie-card__details">   
+      <p class="movie-card__genres">${genresNames} </p>
+      ${
+        release_date
+          ? `<p class="movie-card__year">&nbsp;|&nbsp;${release_date.slice(
+              0,
+              4
+            )}</p>`
+          : ''
+      }
+    
+    ${
+      vote_average
+        ? `<p class="movie-card__rating">${vote_average.toFixed(1)}</p>`
+        : ''
+    }   
+  </div>
+</li>`;
+      }
+    )
+    .join('');
+  return movieCardMarkup;
+}
