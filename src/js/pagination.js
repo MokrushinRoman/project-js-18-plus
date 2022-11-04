c3f09b100bea92258b35a6ef3ff2a1ee2b744b00
Sergey Miniaylo <po4tovuj@gmail.com>
2022-11-04 13:52:46 +0200

    fix pagination maxCallStack, remove 2 default export

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -10,7 +10,7 @@ const fetchParams = {
 
 let paginationButtons = null;
 let currentPage = null;
-let totalPages = "";
+let totalPages = '';
 let maxPages = 10;
 const serchForm = document.querySelector('#search-form');
 const movieList = document.querySelector('.movie-list');
@@ -19,9 +19,9 @@ serchForm.addEventListener('submit', onSerchSubmit);
 
 async function onSerchSubmit(e) {
   e.preventDefault();
-  movieList.innerHTML = "";
+  movieList.innerHTML = '';
   currentPage = 1;
-  pagBtnEl.remove();
+  pagBtnEl?.remove();
   fetchParams.query = serchForm.elements.searchQuery.value.toLowerCase().trim();
   fetchParams.page = currentPage;
   await newMovieSearch(fetchParams);
@@ -32,60 +32,52 @@ const pageNumbers = (total, max, current) => {
   const half = Math.floor(max / 2);
   let to = max;
 
-  if(current + half >= total) {
+  if (current + half >= total) {
     to = total;
-  } else if(current > half) {
-    to = current + half ;
+  } else if (current > half) {
+    to = current + half;
   }
 
   let from = Math.max(to - max, 0);
 
-  return Array.from({length: Math.min(total, max)}, (_, i) => (i + 1) + from);
-}
+  return Array.from({ length: Math.min(total, max) }, (_, i) => i + 1 + from);
+};
 
 function crateNewPage() {
   if (!fetchParams.query) {
-    movieList.innerHTML = "";
+    movieList.innerHTML = '';
     const page = fetchParams.page;
     return nextRandomMovies({ page });
   }
   return nextUserMovies(fetchParams);
-};
-
-
+}
 
 async function nextRandomMovies({ page }) {
-  const result = await getTrending({ page }).then(
-    ({ results }) => {
-    return renderCards(results)
-    }
-  );
+  const result = await getTrending({ page }).then(({ results }) => {
+    return renderCards(results);
+  });
   movieList.innerHTML = result;
 }
 
 async function newMovieSearch(params) {
-  const result = await searchMovies(params).then(
-    ({ results, total_pages }) => {
-      totalPages = total_pages;
-    return renderCards(results)
-
-    }
-  );
-  !result ? Notify.failure("Can't find anything") : movieList.innerHTML = result;
+  const result = await searchMovies(params).then(({ results, total_pages }) => {
+    totalPages = total_pages;
+    return renderCards(results);
+  });
+  !result
+    ? Notify.failure("Can't find anything")
+    : (movieList.innerHTML = result);
 }
 
 async function nextUserMovies(params) {
-  const result = await searchMovies(params).then(
-    ({ results, total_pages }) => {
-      totalPages = total_pages;
-      return  renderCards(results)
-
-    }
-  );
+  const result = await searchMovies(params).then(({ results, total_pages }) => {
+    totalPages = total_pages;
+    return renderCards(results);
+  });
   movieList.innerHTML = result;
 }
 
-export default function PaginationButton(total, current) {
+export function PaginationButton(total, current) {
   totalPages = total;
   currentPage = current;
   const maxPagesVisible = maxPages;
@@ -97,14 +89,19 @@ export default function PaginationButton(total, current) {
     start: () => currentPage === 1 || currentPage > totalPages,
     prev: () => pages[0] === 1,
     end: () => currentPage >= totalPages,
-    next: () => pages.slice(-1)[0] === totalPages
-  }
+    next: () => pages.slice(-1)[0] === totalPages,
+  };
 
   const frag = document.createDocumentFragment();
   const paginationButtonContainer = document.createElement('div');
   paginationButtonContainer.className = 'pagination-buttons';
 
-  const createAndSetupButton = (label = '', cls = '', disabled = false, handleClick) => {
+  const createAndSetupButton = (
+    label = '',
+    cls = '',
+    disabled = false,
+    handleClick
+  ) => {
     const buttonElement = document.createElement('button');
     buttonElement.textContent = label;
     buttonElement.className = `page-btn ${cls}`;
@@ -114,18 +111,21 @@ export default function PaginationButton(total, current) {
       crateNewPage();
       this.update();
       paginationButtonContainer.value = currentPage;
-      paginationButtonContainer.dispatchEvent(new CustomEvent('change', {detail: {currentPageBtn}}));
+      paginationButtonContainer.dispatchEvent(
+        new CustomEvent('change', { detail: { currentPageBtn } })
+      );
     });
 
     return buttonElement;
-  }
+  };
 
-  const onPageButtonClick = e => currentPage = Number(e.currentTarget.textContent);
+  const onPageButtonClick = e =>
+    (currentPage = Number(e.currentTarget.textContent));
 
-  const onPageButtonUpdate = index => (btn) => {
+  const onPageButtonUpdate = index => btn => {
     btn.textContent = pages[index];
 
-    if(pages[index] === currentPage) {
+    if (pages[index] === currentPage) {
       currentPageBtn.classList.remove('active');
       btn.classList.add('active');
       currentPageBtn = btn;
@@ -134,23 +134,35 @@ export default function PaginationButton(total, current) {
   };
 
   buttons.set(
-    createAndSetupButton('', 'start-page', disabled.start(), () => currentPage = 1),
-    (btn) => btn.disabled = disabled.start()
-  )
+    createAndSetupButton(
+      '',
+      'start-page',
+      disabled.start(),
+      () => (currentPage = 1)
+    ),
+    btn => (btn.disabled = disabled.start())
+  );
 
   buttons.set(
-    createAndSetupButton('', 'prev-page', disabled.prev(), () => currentPage -= 1),
-    (btn) => btn.disabled = disabled.prev()
-
-  )
+    createAndSetupButton(
+      '',
+      'prev-page',
+      disabled.prev(),
+      () => (currentPage -= 1)
+    ),
+    btn => (btn.disabled = disabled.prev())
+  );
 
   pages.map((pageNumber, index) => {
     const isCurrentPage = currentPage === pageNumber;
     const button = createAndSetupButton(
-      pageNumber, isCurrentPage ? 'active' : '', false, onPageButtonClick
+      pageNumber,
+      isCurrentPage ? 'active' : '',
+      false,
+      onPageButtonClick
     );
 
-    if(isCurrentPage) {
+    if (isCurrentPage) {
       currentPageBtn = button;
     }
 
@@ -158,14 +170,24 @@ export default function PaginationButton(total, current) {
   });
 
   buttons.set(
-    createAndSetupButton('', 'next-page', disabled.next(), () => currentPage += 1),
-    (btn) => btn.disabled = disabled.next()
-  )
+    createAndSetupButton(
+      '',
+      'next-page',
+      disabled.next(),
+      () => (currentPage += 1)
+    ),
+    btn => (btn.disabled = disabled.next())
+  );
 
   buttons.set(
-    createAndSetupButton('', 'end-page', disabled.end(), () => currentPage = totalPages),
-    (btn) => btn.disabled = disabled.end()
-  )
+    createAndSetupButton(
+      '',
+      'end-page',
+      disabled.end(),
+      () => (currentPage = totalPages)
+    ),
+    btn => (btn.disabled = disabled.end())
+  );
 
   buttons.forEach((_, btn) => frag.appendChild(btn));
   paginationButtonContainer.appendChild(frag);
@@ -173,20 +195,20 @@ export default function PaginationButton(total, current) {
   this.render = (container = document.querySelector('main')) => {
     container.appendChild(paginationButtonContainer);
     pagBtnEl = document.querySelector('.pagination-buttons');
-  }
+  };
 
   this.update = (newPageNumber = currentPage) => {
     currentPage = newPageNumber;
     pages = pageNumbers(totalPages, maxPagesVisible, currentPage);
     buttons.forEach((updateButton, btn) => updateButton(btn));
-  }
+  };
 
-  this.onChange = (handler) => {
+  this.onChange = handler => {
     paginationButtonContainer.addEventListener('change', handler);
-  }
+  };
 }
 
 export default function createPaginations(total, current) {
   paginationButtons = new PaginationButton(total, current);
   paginationButtons.render();
-};
+}
